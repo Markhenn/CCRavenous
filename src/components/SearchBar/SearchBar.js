73d6b5317bbde412535e9d3f393469517602f10c
Mark <markhenningerwork@gmail.com>
2018-04-03 03:17:44 +0200

    add location and term change handler

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,6 +16,8 @@ class SearchBar extends React.Component {
             location: '',
             sortBy: 'best_match',
         };
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind.bind(this);
     }
 
     getSortByClass(sortByOption){
@@ -33,6 +35,14 @@ class SearchBar extends React.Component {
         });
     }
 
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    handleLocationChange(event){
+        this.setState({location: event.target.value});
+    }
+
     render() {
         return (
             <div className="SearchBar">
@@ -42,8 +52,8 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
